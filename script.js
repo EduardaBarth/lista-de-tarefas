68396dd7f03d10d1f46552c222564f23f000857e
Topaza <eduarda.b2004@aluno.ifsc.edu.br>
2023-01-30 14:57:14 -0300

    editar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tarefas = ['html', 'css', 'js'] // []-> array
+const tarefas = [] // []-> array
 
 function add(){
     //const input = document.querySelector("input");
@@ -9,7 +9,7 @@ function add(){
     //    render()
     //}
     //outro jeito de fazer
-    const input = document.querySelector("input");
+    const input = document.querySelector("input")
     const tarefa = input.value
     if (!tarefa) {
         alert("escreva uma tarefa")
@@ -17,18 +17,34 @@ function add(){
     }
     tarefas.push(tarefa) //adiciona nova tarefa
     input.value = "" //barra do input "limpa"
+    
     render()
 }
 
 function render(){ 
     
-    const ol = document.querySelector("ol");
+    const ol = document.querySelector("ol")
     ol.innerHTML = null // o conteúdo html dentro da ol é nulo
     tarefas.forEach(function(tarefa){
-        const li = document.createElement("li"); //criando um novo li
+        const li = document.createElement("li") //criando um novo li
         li.innerText = tarefa //tradução: o texto dentro de li é a tarefa(o valor dentro do input)
         
-        const excluir = document.createElement("button");
+        const input2 = document.createElement("input")
+        li.appendChild(input2)
+
+        const editar = document.createElement("button")
+        editar.innerText="editar"
+        li.appendChild(editar)
+        editar.addEventListener("click", function(){
+            const tarefaNova = input2.value
+            const index = tarefas.indexOf(tarefa);
+            if (index !== -1) {
+                tarefas[index] = tarefaNova;
+            }
+            render()
+        })
+
+        const excluir = document.createElement("button")
         excluir.innerText = "apagar"
         li.appendChild(excluir)
         excluir.addEventListener("click", function(){
@@ -36,10 +52,9 @@ function render(){
             tarefas.splice(apagar,1)
             render()
         });
-        
-        ol.appendChild(li)// fazer a tarefa aparecer dentro da lista
-    })
-   
-}
 
+        ol.appendChild(li)// fazer a tarefa aparecer dentro da lista
+        
+    })
+}
 render()
